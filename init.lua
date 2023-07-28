675ac92335c1e05e0ec63a0871da6215be77bd5a
Noël Mugnier <noel.mugnier@outlook.com>
2023-07-28 17:01:58 +0200

    Add condition to avoid loading plugins when loading neovim from vscode

--- init.lua
+++ init.lua
@@ -1,16 +1,20 @@
-require "config.options"
-require "config.lazy"
-
-if vim.fn.argc(-1) == 0 then
-  vim.api.nvim_create_autocmd("User", {
-    group = vim.api.nvim_create_augroup("NeovimPDE", { clear = true }),
-    pattern = "VeryLazy",
-    callback = function()
-      require "config.autocmds"
-      require "config.keymaps"
-    end,
-  })
+if vim.g.vscode then
+	-- VSCode extensions
 else
-  require "config.autocmds"
-  require "config.keymaps"
+	require("config.options")
+	require("config.lazy")
+
+	if vim.fn.argc(-1) == 0 then
+		vim.api.nvim_create_autocmd("User", {
+			group = vim.api.nvim_create_augroup("NeovimPDE", { clear = true }),
+			pattern = "VeryLazy",
+			callback = function()
+				require("config.autocmds")
+				require("config.keymaps")
+			end,
+		})
+	else
+		require("config.autocmds")
+		require("config.keymaps")
+	end
 end
