01def56eaffba6fedea8c61d911d700441930d3b
Noël Mugnier <noel.mugnier@outlook.com>
2023-09-28 18:22:13 +0200

    Fixed tab lsp indicator

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -14,7 +14,22 @@ return {
 		dependencies = "nvim-tree/nvim-web-devicons",
 		config = {
 			options = {
+				indicator = {
+					icon = "▎", -- this should be omitted if indicator style is not 'icon'
+					style = "icon",
+				},
+				show_tab_indicators = true,
+				show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
+				persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+				separator_style = "thick",
 				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level, diagnostics_dict, context)
+					if level:match("error") and count > 0 then
+						return " "
+					end
+
+					return ""
+				end,
 				offsets = {
 					{
 						filetype = "NvimTree",
