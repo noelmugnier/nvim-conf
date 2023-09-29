05164b1a95445f0b95ea628bbd9587fedd84239a
Noël Mugnier <noel.mugnier@outlook.com>
2023-09-29 14:49:23 +0200

    Fix legendary loading issue

--- lua/plugins/legendary.lua
+++ lua/plugins/legendary.lua
@@ -5,9 +5,15 @@ return {
 		keys = {
 			{ "<leader>ok", "<cmd>Legendary<cr>", desc = "Keybindings" },
 		},
+		dependencies = {
+			{ "nvim-telescope/telescope.nvim" },
+		},
 		opts = {
-			which_key = { auto_register = true },
+			which_key = { auto_register = true, use_groups = true },
 			lazy_nvim = { auto_register = true },
+			extensions = {
+				nvim_tree = true,
+			},
 		},
 	},
 }
